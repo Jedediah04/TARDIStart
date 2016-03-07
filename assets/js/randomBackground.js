48b457b28b06e6b487a5c905eb99ff9a4caa76b9
hydrog3n <loic.vaille@gmail.com>
2016-03-07 23:16:49 +0100

    Refact and recode some function and use bower

---
+++ assets/js/randomBackground.js
@@ -0,0 +1,10 @@
+var bg = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg'];
+
+var randombackground =function() {
+    var nb = Math.floor((Math.random() * (bg.length-1)) + 1);
+    $('body').css('background', "url('/assets/img/"+bg[nb]+"')");
+};
+
+$(function() {
+    randombackground();
+});
