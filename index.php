28ced5b4201c4536a3dc2b36f1ab30705e6f110a
Edouard Patout <edouard@tardisbox.net>
2016-03-07 18:00:55 +0100

    add monitoring/groups/uptime

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 		  $selectedBg = $bg[$i]; // ajout du nom de l'image pour inclusion CSS
 	?>
 
-	<!-- CSS dans header pour backgroud aléatoire -->
+	<!-- CSS dans header pour background aléatoire -->
 	<style type="text/css">
 		html, body{
 			margin: 0; padding: 0;
@@ -52,6 +52,18 @@
 	<script src="/ressources/particles/particles.js"></script>
 	<script src="/ressources/particles/app.js"></script>
 
+	<?php	
+		// UPTIME
+		exec("uptime", $system); // get the uptime stats 
+		$string = $system[0]; // this might not be necessary 
+		$uptime = explode(" ", $string); // break up the stats into an array 
+		$up_days = $uptime[3]; // grab the days from the array 
+		$hours = explode(":", $uptime[6]); // split up the hour:min in the stats 
+		$up_hours = $hours[0]; // grab the hours 
+		$mins = $hours[1]; // get the mins 
+		$up_mins = str_replace(",", "", $mins); // strip the comma from the mins 
+	?>
+
 	<!-- contenu -->
 	<div class="center">
 		<div class="content" id="form" style="top: 0px; opacity: 1;">
@@ -59,111 +71,165 @@
 			<div id="logotop"></div>
 			<!-- recherche -->
 			<span class="fa fa-search"></span>
-			<input type="text" id="q" value="" placeholder="Allons-y..." onkeypress="javascript:handleQuery(event,this.value);" onfocus="this.value=this.value" />
+			<input type="text" id="q" value="" placeholder="<?php echo "Je fonctionne depuis " . $up_days . " jour(s) et " . $up_hours . " heure(s).";?>" onkeypress="javascript:handleQuery(event,this.value);" onfocus="this.value=this.value" />
 			<br>
 			
-			<?php
-				// CPU USAGE
-				$loads = sys_getloadavg();
-				$core_nums = trim(shell_exec("grep -P '^processor' /proc/cpuinfo|wc -l"));
-				$load = round($loads[0]/($core_nums + 1)*100, 2);
-				echo 'CPU : '.$load.'% ';
+			<!-- monitoring -->
+			<div class="monitoring">
+				<div class="CPU">
+					<?php
+						// CPU USAGE
+						$loads = sys_getloadavg();
+						$core_nums = trim(shell_exec("grep -P '^processor' /proc/cpuinfo|wc -l"));
+						$load = round($loads[0]/($core_nums + 1)*100, 0);
+						echo 'CPU : '.$load.'% ';
+						echo "
+						<div class=\"ui-progress-bar ui-container\" id=\"CPUbar\">
+							<div class=\"ui-progress\" style=\"width: $load%;\">
+								<!--<span class=\"ui-label\">$load%</span>-->
+							</div>
+						</div>
+						";
+					?>
+				</div>
 				
-				// RAM USAGE
-				$data = explode("\n", file_get_contents("/proc/meminfo"));
-				$meminfo = array();
-				foreach ($data as $line) {
-					list($key, $val) = explode(":", $line);
-					$meminfo[$key] = trim($val);
-				}
-				$totalRAM = $meminfo[MemTotal];
-				$totalRAM=ereg_replace("[^0-9]","",$totalRAM); 
-				$cachedRAM = $meminfo[Cached];
-				$cachedRAM=ereg_replace("[^0-9]","",$cachedRAM); 
-				$free=$totalRAM-$cachedRAM;
-				echo ' | RAM : '.round((($free/1024)/1024),2).' Go ';
+				<div class="RAM">	
+					<?php
+						// RAM USAGE
+						$data = explode("\n", file_get_contents("/proc/meminfo"));
+						$meminfo = array();
+						foreach ($data as $line) {
+							list($key, $val) = explode(":", $line);
+							$meminfo[$key] = trim($val);
+						}
+						$totalRAM = $meminfo[MemTotal];
+						$totalRAM=ereg_replace("[^0-9]","",$totalRAM); 
+						$cachedRAM = $meminfo[Cached];
+						$cachedRAM=ereg_replace("[^0-9]","",$cachedRAM); 
+						$free=$totalRAM-$cachedRAM;
+						$used = $totalRAM-$free;
+						$percent = ($used/$totalRAM)*100;
+						echo 'RAM : '.round($percent,0).'%';
+						echo "
+						<div class=\"ui-progress-bar ui-container\" id=\"RAMbar\">
+							<div class=\"ui-progress\" style=\"width: $percent%;\">
+								<!--<span class=\"ui-label\">$percent%</span>-->
+							</div>
+						</div>
+						";
+					?>	
+				</div>
 				
-				// HDD USAGE
-				$bytes = disk_free_space("."); 
-				$si_prefix = array( 'o', 'Ko', 'Mo', 'Go', 'To', 'EB', 'ZB', 'YB' );
-				$base = 1024;
-				$class = min((int)log($bytes , $base) , count($si_prefix) - 1);
-				$used = sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
-				$totalRAM=ereg_replace("[^0-9]","",$used); 
-				$bytes = disk_total_space("."); 
-				$si_prefix = array( 'o', 'Ko', 'Mo', 'Go', 'To', 'EB', 'ZB', 'YB' );
-				$base = 1024;
-				$class = min((int)log($bytes , $base) , count($si_prefix) - 1);
-				$total = sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
-				$totalRAM=ereg_replace("[^0-9]","",$total); 
-				$used = $total - $used;
-				echo ' | HDD : '.$used.' Go ';
-				
-				// UPTIME
-				exec("uptime", $system); // get the uptime stats 
-				$string = $system[0]; // this might not be necessary 
-				$uptime = explode(" ", $string); // break up the stats into an array 
-				$up_days = $uptime[3]; // grab the days from the array 
-				$hours = explode(":", $uptime[7]); // split up the hour:min in the stats 
-				$up_hours = $hours[0]; // grab the hours 
-				$mins = $hours[1]; // get the mins 
-				$up_mins = str_replace(",", "", $mins); // strip the comma from the mins 
-				echo " |  UP : " . $up_days . " JOURS ";
-			?>
+				<div class="HDD">			
+					<?php
+						// HDD USAGE
+						$bytes = disk_free_space("."); 
+						$si_prefix = array( 'o', 'Ko', 'Mo', 'Go', 'To', 'EB', 'ZB', 'YB' );
+						$base = 1024;
+						$class = min((int)log($bytes , $base) , count($si_prefix) - 1);
+						$used = sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
+						$totalRAM=ereg_replace("[^0-9]","",$used); 
+						$bytes = disk_total_space("."); 
+						$si_prefix = array( 'o', 'Ko', 'Mo', 'Go', 'To', 'EB', 'ZB', 'YB' );
+						$base = 1024;
+						$class = min((int)log($bytes , $base) , count($si_prefix) - 1);
+						$total = sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
+						$totalRAM=ereg_replace("[^0-9]","",$total); 
+						$freeHDD = $total - $used;
+						$percentHDD = ($freeHDD/$total)*100;
+						echo 'HDD : '.round($percentHDD,0).'%';
+						echo "
+						<div class=\"ui-progress-bar ui-container\" id=\"HDDbar\">
+							<div class=\"ui-progress\" style=\"width: $percentHDD%;\">
+								<!--<span class=\"ui-label\">$load%</span>-->
+							</div>
+						</div>
+						";
+					?>	
+				</div>
+			</div>
 			
 			<!-- liens -->
 			<!-- voir http://fontawesome.io/icons/ pour le nom des icones -->
 			<div id="links">
-				<a href="https://domain.tld/rutorrent" TITLE="ruTorrent">
-					<span class="fa fa-download"></span>
-					<div id="lien">ruTorrent</div>
-				</a>
-				<a href="https://app.plex.tv" TITLE="Plex">
-					<span class="fa fa-play-circle-o"></span>
-					<div id="lien">Plex</div>
-				</a>
-				<a href="https://domain.tld/sickrage/" TITLE="Sickrage">
-					<span class="fa fa-television"></span>
-					<div id="lien"> Sickrage</div>
-				</a>
-				<a href="https://domain.tld/app/glype" TITLE="Proxy">
-					<span class="fa fa-globe"></span>
-					<div id="lien">Proxy</div>
-				</a>
-				<a href="https://webmail.domain.tld" TITLE="Rainloop">
-					<span class="fa fa-envelope"></span>
-					<div id="lien"> Rainloop</div>
-				</a>
-				<a href="https://cloud.domain.tld" TITLE="ownCloud">
-					<span class="fa fa-cloud"></span>
-					<div id="lien">ownCloud</div>
-				</a>
+				<div class="SEEDBOX">
+					<div class="link1">
+						SEEDBOX<br>
+						<img src="/ressources/img/link.png">
+					</div>
+					<a href="https://domain.tld/rutorrent" TITLE="ruTorrent">
+						<span class="fa fa-download"></span>
+						<div id="lien">ruTorrent</div>
+					</a>
+					<a href="https://app.plex.tv/web/app" TITLE="Plex">
+						<span class="fa fa-play-circle-o"></span>
+						<div id="lien">Plex</div>
+					</a>
+					<a href="https://domain.tld/sickrage/" TITLE="Sickrage">
+						<span class="fa fa-television"></span>
+						<div id="lien"> Sickrage</div>
+					</a>
+				</div>
+				
+				<div class="CLOUD">
+					<div class="link2">
+						CLOUD<br>
+						<img src="/ressources/img/link.png">
+					</div>
+					<a href="https://rss.domain.tld" TITLE="Selfoss">
+						<span class="fa fa-rss-square"></span>
+						<div id="lien">Selfoss</div>
+					</a>
+					<a href="https://webmail.domain.tld" TITLE="Rainloop">
+						<span class="fa fa-envelope"></span>
+						<div id="lien"> Rainloop</div>
+					</a>
+					<a href="https://cloud.domain.tld" TITLE="ownCloud">
+						<span class="fa fa-cloud"></span>
+						<div id="lien">ownCloud</div>
+					</a>
+				</div>
+				
 				<br>
 				<br>
-				<a href="https://img.domain.tld/" TITLE="Lutim">
-					<span class="fa fa-file-image-o"></span>
-					<div id="lien">Lutim</div>
-				</a>
-				<a href="https://file.domain.tld/" TITLE="Lufi">
-					<span class="fa fa-file-o"></span>
-					<div id="lien">Lufi</div>
-				</a>
-				<a href="https://domain.tld/app/paste" TITLE="ZeroBin">
-					<span class="fa fa-file-text-o"></span>
-					<div id="lien">Paste</div>
-				</a>
-				<a href="https://rss.domain.tld/" TITLE="Selfoss">
-					<span class="fa fa-rss-square"></span>
-					<div id="lien">Selfoss</div>
-				</a>
-				<a href="https://codiad.domain.tld" TITLE="Codiad">
-					<span class="fa fa-file-code-o"></span>
-					<div id="lien">Codiad</div>
-				</a>
-				<a href="https://shield.domain.tld" TITLE="LibreNMS">
-					<span class="fa fa-bar-chart"></span>
-					<div id="lien">SHIELD</div>
-				</a>
+				
+				<div class="HOSTING">
+					<a href="https://img.domain.tld" TITLE="Lutim">
+						<span class="fa fa-file-image-o"></span>
+						<div id="lien">Lutim</div>
+					</a>
+					<a href="https://file.domain.tld" TITLE="Lufi">
+						<span class="fa fa-file-o"></span>
+						<div id="lien">Lufi</div>
+					</a>
+					<a href="https://paste.domain.tld" TITLE="ZeroBin">
+						<span class="fa fa-file-text-o"></span>
+						<div id="lien">Paste</div>
+					</a>
+					<div class="link3">
+						<img src="/ressources/img/link_rev.png"><br>
+						HOSTING
+					</div>
+				</div>
+				
+				<div class="MISC">
+					<a href="https://domain.tld/app/glype" TITLE="Proxy">
+						<span class="fa fa-globe"></span>
+						<div id="lien">Proxy</div>
+					</a>
+					<a href="https://codiad.domain.tld" TITLE="Codiad">
+						<span class="fa fa-file-code-o"></span>
+						<div id="lien">Codiad</div>
+					</a>
+					<a href="https://shield.domain.tld" TITLE="LibreNMS">
+						<span class="fa fa-bar-chart"></span>
+						<div id="lien">SHIELD</div>
+					</a>
+					<div class="link4">
+						<img src="/ressources/img/link_rev.png"><br>
+						MISC.
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
