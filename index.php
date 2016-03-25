1a4b1907c04aef2013ff861cfa99b4f7dd7890c0
Léonard Cherouvrier <leonard.cherouvrier@icloud.com>
2016-03-25 19:48:31 +0100

    Adaptation mobile
    Règles CSS pour rendre TARDISTart compatible sur mobile (et changement
    du meta viewport dans l’index)

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<link rel="icon" type="ico" href="./assets/img/tardis.ico">
 	<link rel="stylesheet" href="./assets/bower/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
 <body>
